976fedcd7be3b98ff4eaff429a6c7d82aa81f533
NguyenPatrick <ultimpact@gmail.com>
2018-11-26 17:16:08 -0500

    Added and removed sprites

---
+++ Assets/FruitNinja/Script/Expand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expand : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start () 
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+
+	void Update () 
+    {
+    
+       animator.Play("Expansion");
+    }
+
+}
