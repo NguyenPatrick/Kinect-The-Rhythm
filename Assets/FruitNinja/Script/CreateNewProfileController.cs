269764a73117bc4f299bf2bf6153d5b23062ad67
AnthonyLaiRH <lai.anthony.rh@gmail.com>
2018-11-27 20:24:04 -0500

    Profile Pages

---
+++ Assets/FruitNinja/Script/CreateNewProfileController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class CreateNewProfileController : MonoBehaviour {
+
+    public InputField firstNameInput;
+    public InputField lastNameInput;
+    public Dropdown gameTypeDropdown;
+    public Button createProfileButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClickCreateProfileButton()
+    {
+        ProfileController.profiles.Add()
+    }
+
+}
---
+++ Assets/FruitNinja/Script/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileController : MonoBehaviour {
+    public static List<string> profiles;
+
+    public List<GameObject> profileGameObjects;
+
+	// Use this for initialization
+	void Start () {
+        profileGameObjects = new List<GameObject>();
+        PopulateProfileList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PopulateProfileList()
+    {
+        
+    }
+}
